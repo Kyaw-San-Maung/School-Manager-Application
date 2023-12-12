8ea73eeec5c1a8b3c64864b9eaa23e625645a44f
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-12 14:05:39 +0630

    installing react-icons in my application

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,18 @@
 
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-icons/io5'
+import { Navbar, NavbarBrand } from 'react-bootstrap'
+import { IoSchoolOutline } from 'react-icons/io5'
 
 export default function Dashboard() {
   return (
       <>
-          
+          <Navbar color='dark' light mb-2>
+              <NavbarBrand className='textwhite'>
+                  <IoSchoolOutline className='font-size-xxl'/>
+              </NavbarBrand>
+          </Navbar>
       </>
   )
 }
