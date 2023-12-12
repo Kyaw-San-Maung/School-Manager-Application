7d219e7362a94822c5051f72ee9ca2d1b8348dd0
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-12 14:30:12 +0630

    Adding card to my applicationby using bootstrap card.

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-icons/io5";
-import { Navbar, NavbarBrand } from "react-bootstrap";
-import { IoSchoolOutline } from "react-icons/io5";
+import { Button, Card, CardText, CardTitle, Col, Container, Navbar, NavbarBrand, Row } from "react-bootstrap";
+import { IoMan, IoSchoolOutline } from "react-icons/io5";
 
 export default function Dashboard() {
   return (
@@ -12,7 +12,24 @@ export default function Dashboard() {
           <IoSchoolOutline className="font-size-xxl" />
           <span className="font-size-l ml-3">School Manager Application</span>
         </NavbarBrand>
-      </Navbar>
+          </Navbar>
+          <Container className="mt-3">
+              <Row>
+                  <Col sm='4'>
+                      <Card body>
+                          <CardTitle tag='h5'>
+                              <IoMan className="font-size-xl" /> 85 students
+                          </CardTitle>
+                          <CardText>
+                              With Supporting text below as a natural lead-in to additional content.
+                          </CardText>
+                          <Button>Manage Student</Button>
+                      </Card>
+                  </Col>
+                  <Col sm='4'></Col>
+                  <Col sm='4'></Col>
+              </Row>
+          </Container>
     </>
   );
 }
