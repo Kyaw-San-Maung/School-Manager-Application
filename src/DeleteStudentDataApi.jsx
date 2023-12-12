6981cf3b91b024224b85f7365c90e4a1ba80721b
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-13 02:15:27 +0630

    Finish my create, delete, and getting all data from API feature.

--- src/DeleteStudentDataApi.jsx
+++ src/DeleteStudentDataApi.jsx
@@ -1,10 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
 
-
-const Api_url = "http://localhost:8080/delete"
+const Api_url = "http://localhost:8080/delete";
 
 export default function deleteStudentData(id) {
   return (
-    axios.post(Api_url +'/'+ id)
-  )
+    //finish my delete feature
+    axios.post(Api_url + "/" + id)
+  );
 }
