ce391d65585b31faff42b51a6a64dbb1af048ff3
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-12 14:44:16 +0630

    Adding button for my applicaiton.

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-icons/io5";
-import { Button, Card, CardText, CardTitle, Col, Container, Navbar, NavbarBrand, Row } from "react-bootstrap";
-import { IoMan, IoSchoolOutline } from "react-icons/io5";
+import {
+  Button,
+  Card,
+  CardText,
+  CardTitle,
+  Col,
+  Container,
+  Navbar,
+  NavbarBrand,
+  Row,
+} from "react-bootstrap";
+import { IoMan, IoSchool, IoSchoolOutline, IoSettings } from "react-icons/io5";
+import "./Dashboard.css";
 
 export default function Dashboard() {
+    const [student, setSutdent] = useState([]);
   return (
     <>
       <Navbar bg="dark" light mb-2>
@@ -12,23 +24,60 @@ export default function Dashboard() {
           <IoSchoolOutline className="font-size-xxl" />
           <span className="font-size-l ml-3">School Manager Application</span>
         </NavbarBrand>
-          </Navbar>
-          <Container className="mt-3">
-              <Row>
-                  <Col sm='4'>
-                      <Card body>
-                          <CardTitle tag='h5'>
-                              <IoMan className="font-size-xl" /> 85 students
-                          </CardTitle>
-                          <CardText>
-                              With Supporting text below as a natural lead-in to additional content.
-                          </CardText>
-                          <Button>Manage Student</Button>
-                      </Card>
-                  </Col>
-                  <Col sm='4'></Col>
-                  <Col sm='4'></Col>
-              </Row>
+      </Navbar>
+      <Container className="mt-3">
+        <Row>
+          <Col sm="4">
+            <Card body>
+              <CardTitle tag="h5">
+                <IoMan className="font-size-xl" /> 85 Students
+              </CardTitle>
+              <CardText>
+                With Supporting text below as a natural lead-in to additional
+                content.
+              </CardText>
+              <Button className="btn">Manage Student</Button>
+            </Card>
+          </Col>
+          <Col sm="4">
+            <Card body>
+              <CardTitle tag="h5">
+                <IoSchool className="font-size-xl" /> 20 Teachers
+              </CardTitle>
+              <CardText>
+                With Supporting text below as a natural lead-in to additional
+                content.
+              </CardText>
+              <Button>Manage Student</Button>
+            </Card>
+          </Col>
+          <Col sm="4">
+            <Card body>
+              <CardTitle tag="h5">
+                <IoSettings className="font-size-xl" /> 13 Employees
+              </CardTitle>
+              <CardText>
+                With Supporting text below as a natural lead-in to additional
+                content.
+              </CardText>
+              <Button>Manage Student</Button>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container className="mt-4">
+        <Row>
+          <Col sm="12">
+            <Button className="btn">
+              <span className="font-size-l">Create New Student</span>
+            </Button>
+          </Col>
+        </Row>
+          </Container>
+          
+          <Container className="mt-4">
+              
           </Container>
     </>
   );
---
+++ src/GetAllStudents.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+import React from 'react'
+
+export default function GetAllStudents() {
+  return (
+      <>
+          
+      </>
+  )
+}
