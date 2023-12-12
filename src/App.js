cd6ee0945f41d58e7342a0fde1a5dd39a06aa697
Kyaw San Maung <kyawsanmaung262019@gmail.com>
2023-12-12 14:10:59 +0630

    Creating Navbar for my School Manager application project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <>
-    <h1>Hello World!</h1>
+    <Dashboard/>
     </>
   )
 }
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,18 +1,18 @@
-
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'react-icons/io5'
-import { Navbar, NavbarBrand } from 'react-bootstrap'
-import { IoSchoolOutline } from 'react-icons/io5'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-icons/io5";
+import { Navbar, NavbarBrand } from "react-bootstrap";
+import { IoSchoolOutline } from "react-icons/io5";
 
 export default function Dashboard() {
   return (
-      <>
-          <Navbar color='dark' light mb-2>
-              <NavbarBrand className='textwhite'>
-                  <IoSchoolOutline className='font-size-xxl'/>
-              </NavbarBrand>
-          </Navbar>
-      </>
-  )
+    <>
+      <Navbar bg="dark" light mb-2>
+        <NavbarBrand className="text-white">
+          <IoSchoolOutline className="font-size-xxl" />
+          <span className="font-size-l ml-3">School Manager Application</span>
+        </NavbarBrand>
+      </Navbar>
+    </>
+  );
 }
